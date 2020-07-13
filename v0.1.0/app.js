f47cba59102049538adc29010eeb2eddf6fe2b80
GoatMe <andy.gavrilov@gmail.com>
2020-07-13 20:03:40 +0100

    Update YelpCamp to version v0.2.0

--- v0.1.0/app.js
+++ v0.1.0/app.js
@@ -1,23 +1,25 @@
-var express = require("express");
-var app = express();
+var express = require("express"),
+	app = express(),
+	mongoose = require("mongoose"),
+	bodyParser = require("body-parser");
 
-var bodyParser = require("body-parser");
+mongoose.connect("mongodb://localhost:27017/yelp_camp", { useNewUrlParser: true,
+   useUnifiedTopology: true });
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
 app.set("view engine", "ejs");
 
-/*===============================================*/
-	var campgrounds = 
-	[
-		{name: "Salmon Flake" , img: "https://images.unsplash.com/photo-1502993194517-7734018a0d8b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1191&q=80"},
-		{name: "Eye Openner" , img: "https://images.unsplash.com/photo-1492648272180-61e45a8d98a7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80"},
-		{name: "Stay Lost" , img: "https://images.unsplash.com/photo-1562413255-9d8f3188f197?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80"},
-		{name: "Fill Me" , img: "https://images.unsplash.com/photo-1573145893063-13ae8d51e195?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60"},
-		{name: "Too High" , img: "https://images.unsplash.com/photo-1503265192943-9d7eea6fc77a?ixlib=rb-1.2.1&auto=format&fit=crop&w=967&q=80"}
-	];
+/*====================SCHEMA===========================*/
+var campgroundSchema = new mongoose.Schema({
+	name: String,
+	img: String,
+	desc: String
+});
 
+var Campground = mongoose.model("Campground", campgroundSchema);
+/*====================ROUTES===========================*/
 app.get('/', function(request, response){
 	response.render("landing");
 
@@ -25,23 +27,47 @@ app.get('/', function(request, response){
 
 app.get('/campgrounds', function(request, response){
 
-	response.render("campgrounds", {cg: campgrounds});
+	Campground.find({}, function(err, allCamps){
+		if (err) {
+			console.log(err);
+		}else{
+			response.render("index", {cg: allCamps});
+		}
+	});
 });
 
 app.post('/campgrounds', function(request, response){
 	//Get data from form and add to campgrounds array
 	var name = request.body.name;
 	var img = request.body.img;
-	var newCg = {name: name, img: img};
-	campgrounds.push(newCg);
-	response.redirect("/campgrounds");
+	var desc = request.body.desc;
+	var newCg = {name: name, img: img, desc: desc};
+	//Crate a new Camp data and save to DB
+	Campground.create(newCg, function(err, newCamp){
+		if (err) {
+			console.log("Ooopsy!!!", err);
+		} else {
+			//redirect back to campgrounds page
+			response.redirect('/campgrounds');
+		}
+	});
 });
 
 app.get('/campgrounds/new', function(request, response){
 	response.render("new.ejs");
 });
 
-/*===============================================*/
+app.get('/campgrounds/:id', function (request, response) {
+	Campground.findById(request.params.id, function(err, findCamp){
+		if (err) {
+
+		}else{
+			response.render("show", {cg: findCamp});
+		}
+	});
+});
+
+/*===================CONNECTION============================*/
 app.listen(3000, function () {
 	console.log("YelpCamp Started!");
 });
