ed1da23cf0eebc066f27ef8b5fbf09c47f8a76ff
GoatMe <andy.gavrilov@gmail.com>
2020-07-13 20:12:02 +0100

    Update YelpCamp to version v0.3.0

--- v0.1.0/app.js
+++ v0.1.0/app.js
@@ -1,7 +1,11 @@
 var express = require("express"),
 	app = express(),
 	mongoose = require("mongoose"),
-	bodyParser = require("body-parser");
+	bodyParser = require("body-parser"),
+	Campground = require("./modules/campground"),
+	seedDB = require("./seeds")/*,
+	User = require("user")*/;
+
 
 mongoose.connect("mongodb://localhost:27017/yelp_camp", { useNewUrlParser: true,
    useUnifiedTopology: true });
@@ -11,20 +15,15 @@ app.use(express.static("public"));
 
 app.set("view engine", "ejs");
 
-/*====================SCHEMA===========================*/
-var campgroundSchema = new mongoose.Schema({
-	name: String,
-	img: String,
-	desc: String
-});
+seedDB(); /*For deleting and repopulating the DB on each Server start*/
 
-var Campground = mongoose.model("Campground", campgroundSchema);
 /*====================ROUTES===========================*/
 app.get('/', function(request, response){
 	response.render("landing");
 
 });
 
+//INDEX ROUTE - show all campgrounds
 app.get('/campgrounds', function(request, response){
 
 	Campground.find({}, function(err, allCamps){
@@ -53,16 +52,20 @@ app.post('/campgrounds', function(request, response){
 	});
 });
 
+//NEW ROUTE - show form to create new campground
 app.get('/campgrounds/new', function(request, response){
 	response.render("new.ejs");
 });
 
+// SHOW ROUTE - shows more info about one campground
 app.get('/campgrounds/:id', function (request, response) {
-	Campground.findById(request.params.id, function(err, findCamp){
+	//finding comments by id -> populating comments -> with .exec executing the function
+	Campground.findById(request.params.id).populate("comments").exec(function(err, findCamp){
 		if (err) {
-
+			console.log(err);
 		}else{
 			response.render("show", {cg: findCamp});
+			// console.log(findCamp);
 		}
 	});
 });
